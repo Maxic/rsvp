b11b8f0bf346985acc50be6da2870ab2976bed3a
Barend <barend.poot@gmail.com>
2018-04-21 20:13:17 +0200

    Removed some stuff

--- rsvp/forms.py
+++ rsvp/forms.py
@@ -1,14 +1,10 @@
 from django.forms import ModelForm
 from .models import Attendee
-from django.utils.translation import ugettext_lazy as _
 
 
 class Attendeeform(ModelForm):
     class Meta:
         model = Attendee
         fields = ['name', 'email', 'websiteAddress', 'role', 'takePart', 'additionalMessage']
-        labels = {
-            'takePart': _('Do you want to take part in a physical drawing estafette on the day of the opening?'),
-        }
 
 
